46d63054b03e57eb53228b8f338e759c9112f3a9
github username <github email address>
2018-10-19 20:40:20 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,6 +16,10 @@ class PigLatinizer
           @new_word = new = word.split("").drop(1).join
           @suffix = @first + "way"
           @new_word + @suffix.downcase
+        elsif word.split("")[0..2].join == "Str"
+          @first = word.split("")[0..2]
+          @new_word = new = word.split("").drop(3).join
+          @new_word + @first.join + "ay"
         elsif word.split("")[0..2].join.scan(/[BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz]/).length == 3
           @first = word.split("")[0..2]
           @new_word = new = word.split("").drop(3).join
